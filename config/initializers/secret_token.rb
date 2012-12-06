6f294f8a774629f8f17608942eefb7db5a4b1fe4
Jason Meller <jason.meller@gmail.com>
2012-12-06 14:27:59 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MeillerCom::Application.config.secret_token = '1db00032842cace0769570c8db95c57fb22c2488a7a0beea9494c27519fabb35cd57aa3baa68d90b37ab55463da15c1954cc6c5a23eb1b9e95b3dadf7c6712a3'
